e11ca71aff6a6fc8747075273e8ea95489f0dbbd
aldiinugroho <nugroho.aaldi@gmail.com>
2022-12-04 11:50:21 +0700

    new

---
+++ swifties/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  swifties
+//
+//  Created by aldinugroho on 04/12/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    let appColor = UIColor(named: "softpink")
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = appColor
+        window?.rootViewController = ViewController()
+        return true
+    }
+}
+
